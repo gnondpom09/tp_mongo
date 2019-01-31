e7da23fe683d30aa7980152fa726fa429ca41e0c
cld <cld@orange.fr>
2019-01-31 13:46:08 +0100

    maint

--- maintenance.php
+++ maintenance.php
@@ -10,7 +10,7 @@
         <div>
             <form action="" method="post" id="update-form">
                 <div id="intro">
-                    <p>Veuillez saisir la collection à modifier :</p>
+                    <p>Veuillez sélectionner la collection à modifier :</p>
                 </div>
                 <p class="r_collection">
                 <!-- <div id="r_collection"> -->
@@ -25,39 +25,76 @@
                 </p>
 
                 <p class="data_rech">
-                    <label for="label_data">Zonde de recherche</label>
-                    <input type="data" name="data_rech" id="data_rech" placeholder="saisir votre recherche">
+                    <label for="label_data">Saisir votre recherche</label>
+                    <input type="data" name="data_rech" id="data_rech" size="33<" placeholder="saisir nom ville ou département ou région">
                 </p>
 
-                <!-- <input type="submit" value="Soumettre" name="recherche"> -->
+                <!-- <button class="hollow button rounded bordered" type="submit" href="#">Envoyez</button> -->
+                <input type="submit" value="envoyer" name="recherche">
 
-                <button class="hollow button rounded bordered" type="submit" href="#">Envoyez</button>
-                 
+                <?php
 
+
+                $flag_ville  = 0;
+                $flag_dept   = 0;
+                $flag_region = 0;
+
+                 // select collection 
+                if (!isset($_POST['r_ville']) && !isset($_POST['r_dept']) && !isset($_POST['r_region'])) :
+                    echo "Sélectionner au moins une collection.";
+                else :
+
+                    // flag collection
+                    if (isset($_POST['r_ville']) && !isset($_POST['r_dept']) && !isset($_POST['r_region'])) :
+                        $flag_ville = 1;
+                    else:
+                        if (!isset($_POST['r_ville']) && isset($_POST['r_dept']) && !isset($_POST['r_region'])) :
+                            $flag_dept = 1;
+                        else :
+                            if (!isset($_POST['r_ville']) && !isset($_POST['r_dept']) && isset($_POST['r_region'])) :
+                                $flag_region = 1;
+                                else :
+                                    echo "Sélectionner une seule collection.";
+                            endif;    
+                        endif;
+                    endif;
+                endif;
+
+                 // select data 
+                if (isset($_POST['data_rech'])) :
+
+                    // get attribut collection
+                    $data_rech    = htmlspecialchars($_POST['data_rech']);
+                                                
+                    // get data from collection
+                                    
+                    $filter = [
+                        'nom' => $data_rech
+                    ];
+
+                    // Check ville /département/région: 
+                    $query             = new MongoDB\Driver\Query($filter);
+
+                    if( $flag_ville == 1 ) : 
+                            $result    = $manager->executeQuery($db . '.villes', $query);
+                    else :
+                        if( $flag_dept == 1 ) :
+                            $result    = $manager->executeQuery($db . '.departements', $query);
+                        else :
+                            $result    = $manager->executeQuery($db . '.region', $query);    
+                        endif;
+                    endif;
+
+                    $data_collection = current($result->toArray());
+
+                    print_r($data_collection);
+
+                else :
+                    // display error 
+                    echo "Formulaire incomplet";
+                endif;
+                    
                 
-                <!-- // require_once('config.php');
-                // $flag    = isset($_GET['flag'])?intval($_GET['flag']):0;
-                // $message ='';
-                // if($flag){
-                // $message = $messages[$flag];
-                // }
-                // $filter = [];
-                // $options = [
-                //     'sort' => ['_id' => -1],
-                // ];
-                // $query = new MongoDB\Driver\Query($filter, $options);
-                // $cursor = $manager->executeQuery('onlinestore.products', $query);
-                
-                // if (isset($_POST['ville']) )
-                // { -->
-    
-                <!-- //     }
-                // else
-                // {
-
-                // echo 'Il faut renseigner un nom de ville ou un code département ou un code région !';
-
-                // }
 
                 // $db->villes->find(array("cp" => $update_cp), array("cp" => 1, "pop" => 1));
 
@@ -70,14 +107,8 @@
 
                 // $result = $manager->executeBulkWrite('db.collection', $bulk);
 
-                ?> -->
-
-
-             
+                ?> 
         
-
-        
-    
             </form>
         </div>
     </div>
