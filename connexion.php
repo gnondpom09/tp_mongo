ee7c6f66354b07b702ded8fccb7696b63312d14e
cld <cld@orange.fr>
2019-01-31 09:24:41 +0100

    maj connexion

--- connexion.php
+++ connexion.php
@@ -3,18 +3,13 @@
 // Connexion base geo_france
 
 $dsn='mongodb://localhost:27017';
-$dbname = 'geo_france';
+
 
 try {
-    
         // connexion base de données
-        $Connexion = new MongoDB\Driver\Manager($dsn);  
-
-        // Sélection de la database "geo_france"
-        //  $db = $connexion->selectDB($dbname);
-         
+        $manager = new \MongoDB\Driver\Manager($dsn);
+        $db = "geo_france";
    
 } catch (exception $exep) {
     printf("<p>Erreur : %s</p>\n", htmlspecialchars($exep->getMessage()));
   }
-  
--- maintenance.php
+++ maintenance.php
@@ -2,73 +2,94 @@
 	include 'header.php'; 
 ?>
 
-		<!-- Center Text -->
-		<div class="grid-x"> <!-- need to add justify-center: center in css -->
-			<div class="cell small-4">GEO FRANCE</div>
-		</div>
+<!-- Menu -->
+<div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle></button>
+    <div class="title-bar-title">Menu</div>
+</div>
+<div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top hinge-out-from-top">
+    <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">LDNR</li>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Visualisation</a></li>
+            <li><a href="#">Maintenance</a></li>
+        </ul>
+    </div>
+</div>
 
-	    <!-- Menu -->
-		<div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
-  			<button class="menu-icon" type="button" data-toggle></button>
- 			 <div class="title-bar-title">Visualisation d'une ville</div>
-		</div>
+<div class="modal">
+    <div class="modal__inner">
 
-		<div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top hinge-out-from-top">
-  			<div class="top-bar-left">
-				<ul class="dropdown menu" data-dropdown-menu>
-					<li class="menu-text">LDNR</li>
-					<li><a href="#">Accueil</a></li>
-					<li><a href="#">Visualisation</a></li>
-					<li><a href="#">Maintenance</a></li>
-				</ul>
-  			</div>
-		</div>
+        <form action="" method="post" id="update-form">
 
-<       <div id="main">
             <div id="intro">
                 <p>Veuillez saisir la collection à modifier :</p>
             </div>
 
             <div id="buttons_type">
-                <button id="ville" type="button" onclick="">ville</button>
-                <button id="dept" type="button" onclick="">dept</button>
-                <button id="region" type="button" onclick="">region</button>
+                <button id="maj_ville" type="button" onclick="">ville</button>
+                <button id="maj_dept" type="button" onclick="">dept</button>
+                <button id="maj_region" type="button" onclick="">region</button>
             </div>
 
-            <form id="form">
+            <p class="ville_recherche">
+                <label for="ville_rech">Ville</label>
+                <input type="ville" name="ville" id="ville" placeholder="saisir la ville">
+            </p>
 
-                <div id="infos_area">
-                    
-                    <div id="input_area">
-                        <div>
-                            <input type="data" id="input_data" name="input_data" placeholder="">
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                <div id="update_ville" type="text">
-                    
-                </div>
-
-               
-            </form>
-        </div>
-        
-
-<?php
-
-// // Sélection de la collection villes
-// $c_villes = new MongoCollection($db, "villes");
+            <input type="submit" value="Soumettre" name="recherche">
 
 
 
-//  // sélectionner ville si département et region rempli :
-//  $get_villes = $c_villes->find({nom: "nom_ville", _id_dept: "c_dept", _id_region:"c_region"});
 
+
+            <!-- recherche ville : -->
+            <?php
+
+
+if (isset($_GET['ville']) )
+
+{
+
+    db.villes.find();
+    }
+
+}
+
+else
+
+{
+
+   echo 'Il faut renseigner un nom de ville ou un code département ou un code région !';
+
+}
+
+
+            $db->villes->find(array("cp" => $update_cp), array("cp" => 1, "pop" => 1));
+
+            ?>
+
+
+            <!-- mise à jour dans la collection ville : -->
+
+            <?php
+
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->update(
+    ['cp' => $update_cp],
+    ['$set' => ['y' => 3]],
+    ['multi' => false, 'upsert' => false]
+);
+
+$result = $manager->executeBulkWrite('db.collection', $bulk);
 
 ?>
 
+
+        </form>
+    </div>
+</div>
 
 <?php 
 	include 'footer.php'; 
