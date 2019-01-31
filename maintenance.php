4bafa50323ce62f1f4e3686bb082ec3625e6af37
cld <cld@orange.fr>
2019-01-31 12:22:16 +0100

    corrigé

--- maintenance.php
+++ maintenance.php
@@ -2,80 +2,86 @@
 	include 'header.php'; 
 ?>
 
-<!-- Menu -->
-<div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
-    <button class="menu-icon" type="button" data-toggle></button>
-    <div class="title-bar-title">Menu</div>
-</div>
+	<!-- Container background transparent 40% -->
+    <div class="grid-x grid-padding-x align-spaced">
 
-<div class="maint">
-    <div class="maint_update">
+        <!-- Form -->
+        <!-- <div class="cell medium-4 medium-cell-block-y"> -->
+        <div>
+            <form action="" method="post" id="update-form">
+                <div id="intro">
+                    <p>Veuillez saisir la collection à modifier :</p>
+                </div>
+                <p class="r_collection">
+                <!-- <div id="r_collection"> -->
+                    <input type="radio" name="r_ville" id="-_ville">
+                    <label for="l_ville">Ville</label>
+                    <input type="radio" name="r_dept" id="r_dept">
+                    <label for="l_dept">Département</label>
+                    <input type="radio" name="r_region" id="r_region">
+                    <label for="l_region">Région</label>
+                
+                <!-- </div> -->
+                </p>
 
-        <form action="" method="post" id="update-form">
+                <p class="data_rech">
+                    <label for="label_data">Zonde de recherche</label>
+                    <input type="data" name="data_rech" id="data_rech" placeholder="saisir votre recherche">
+                </p>
 
-            <div id="intro">
-                <p>Veuillez saisir la collection à modifier :</p>
-            </div>
+                <!-- <input type="submit" value="Soumettre" name="recherche"> -->
 
-            <div id="r_collection">
-                <label for="l_ville">Ville</label>
-                <input type="radio" name="r_ville" id="-_ville">
-                <label for="l_dept">Département</label>
-                <input type="radio" name="r_dept" id="r_dept">
-                <label for="l_region"Région></label>
-                <input type="radio" name="r_region" id="r_region">
-            </div>
+                <button class="hollow button rounded bordered" type="submit" href="#">Envoyez</button>
+                 
 
-            <p class="ville_recherche">
-                <label for="ville_rech">Ville</label>
-                <input type="ville" name="ville" id="ville" placeholder="saisir la ville">
-            </p>
+                
+                <!-- // require_once('config.php');
+                // $flag    = isset($_GET['flag'])?intval($_GET['flag']):0;
+                // $message ='';
+                // if($flag){
+                // $message = $messages[$flag];
+                // }
+                // $filter = [];
+                // $options = [
+                //     'sort' => ['_id' => -1],
+                // ];
+                // $query = new MongoDB\Driver\Query($filter, $options);
+                // $cursor = $manager->executeQuery('onlinestore.products', $query);
+                
+                // if (isset($_POST['ville']) )
+                // { -->
+    
+                <!-- //     }
+                // else
+                // {
 
-            <input type="submit" value="Soumettre" name="recherche">
+                // echo 'Il faut renseigner un nom de ville ou un code département ou un code région !';
+
+                // }
+
+                // $db->villes->find(array("cp" => $update_cp), array("cp" => 1, "pop" => 1));
+
+                // $bulk = new MongoDB\Driver\BulkWrite;
+                // $bulk->update(
+                // ['cp' => $update_cp],
+                // ['$set' => ['y' => 3]],
+                // ['multi' => false, 'upsert' => false]
+                // );
+
+                // $result = $manager->executeBulkWrite('db.collection', $bulk);
+
+                ?> -->
 
 
-
-
-
-            <!-- recherche ville : -->
-            <?php
-
-
-    // if (isset($_POST['ville']) )
-    // {
+             
         
-    //     }
-    // else
-    // {
 
-    // echo 'Il faut renseigner un nom de ville ou un code département ou un code région !';
-
-    // }
-
-    //             $db->villes->find(array("cp" => $update_cp), array("cp" => 1, "pop" => 1));
-
-    ?>
-
-
-            <!-- mise à jour dans la collection ville : -->
-
-            <?php
-
-$bulk = new MongoDB\Driver\BulkWrite;
-$bulk->update(
-    ['cp' => $update_cp],
-    ['$set' => ['y' => 3]],
-    ['multi' => false, 'upsert' => false]
-);
-
-$result = $manager->executeBulkWrite('db.collection', $bulk);
-
-?>
-
-
-        </form>
+        
+    
+            </form>
+        </div>
     </div>
-</div>
+
 
 <?php 
 	include 'footer.php'; 
